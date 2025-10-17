74f2cc0cb533316e8ffa56fff8d93e5c605e013c
Ezetorc <ezetorc@gmail.com>
2025-10-16 21:47:46 -0300

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: vercel(),
   output: "server",
   integrations: [react()],
-  
 });
