07b52087c0eca08bf79d32038625c37ac0e341e6
Ezetorc <ezetorc@gmail.com>
2025-10-17 22:03:02 -0300

    Add debug #3

--- src/configuration/public-env.configuration.ts
+++ src/configuration/public-env.configuration.ts
@@ -1,4 +1,2 @@
 export const BASE_URL =
   import.meta.env.PUBLIC_API_URL || "http://localhost:4321";
-console.log(".env: ", import.meta.env.PUBLIC_API_URL);
-console.log("BASE_URL = ", BASE_URL);
--- src/services/articles.service.ts
+++ src/services/articles.service.ts
@@ -37,9 +37,10 @@ export class ArticlesService {
   }
 
   static async getAll(amount: number = 4): Promise<Article[]> {
-    console.log("api url: ", `${this.API_BASE}?amount=${amount}`);
-
     const res = await fetch(`${this.API_BASE}?amount=${amount}`);
+
+    console.log("response: ", res);
+    console.log("success: ", res.ok);
 
     if (!res.ok) return [];
     const data = await res.json();
