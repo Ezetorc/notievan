be6113da56956c6ec66b34b520ad6974b1e75716
Ezetorc <ezetorc@gmail.com>
2025-10-17 21:56:38 -0300

    Add debug #2

--- src/configuration/public-env.configuration.ts
+++ src/configuration/public-env.configuration.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = import.meta.env.PUBLIC_API_URL || "http://localhost:4321";
+export const BASE_URL =
+  import.meta.env.PUBLIC_API_URL || "http://localhost:4321";
+console.log(".env: ", import.meta.env.PUBLIC_API_URL);
+console.log("BASE_URL = ", BASE_URL);
