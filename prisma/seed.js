612ace9a1c87197c679b0dc238d081f504ffe466
Ezetorc <ezetorc@gmail.com>
2025-10-17 21:49:21 -0300

    Migrate to PostgreSQL

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,14 +3,26 @@ import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.article.deleteMany()
+  await prisma.article.deleteMany();
 
-  await prisma.user.update({
-    where: {
-      email: "ezetorc@gmail.com",
-    },
+  const user = await prisma.user.create({
     data: {
       role: "AUTHOR",
+      email: "ezetorc@gmail.com",
+      name: "Ezequias Torczuk",
+      password: "password123",
+    },
+  });
+
+  await prisma.article.create({
+    data: {
+      authorId: user.id,
+      content: "Contenido",
+      description: "Descripción",
+      subtitle: "Subtitulo",
+      title: "Título",
+      thumbnailAlt: "Alt",
+      thumbnailUrl: "https://stock.adobe.com/search?k=%22n+alphabet%22",
     },
   });
 }
