0a647dec75ba9baad1e35dd4ca2c4964325fd0a8
Ezetorc <ezetorc@gmail.com>
2025-10-20 14:45:13 -0300

    Fix bug

--- src/components/SecondaryArticles.tsx
+++ src/components/SecondaryArticles.tsx
@@ -25,29 +25,31 @@ export function SecondaryArticles() {
     if (hasMore) setPage((prev) => prev + 1);
   };
 
-  return (
-    <section>
-      <main
-        className="w-full min-h-[650px] grid
+  if (articles.length > 0) {
+    return (
+      <section>
+        <main
+          className="w-full min-h-[650px] grid
            mobile:grid-rows-4 mobile:grid-cols-1
            desktop:grid-rows-1 desktop:grid-cols-4
            gap-8"
-      >
-        {articles.map((article) => (
-          <SmallArticle key={article.id} article={article} />
-        ))}
-      </main>
+        >
+          {articles.map((article) => (
+            <SmallArticle key={article.id} article={article} />
+          ))}
+        </main>
 
-      {hasMore && (
-        <div className="mt-8 text-center">
-          <button
-            className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-            onClick={handleLoadMore}
-          >
-            Cargar más
-          </button>
-        </div>
-      )}
-    </section>
-  );
+        {hasMore && (
+          <div className="mt-8 text-center">
+            <button
+              className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+              onClick={handleLoadMore}
+            >
+              Cargar más
+            </button>
+          </div>
+        )}
+      </section>
+    );
+  }
 }
