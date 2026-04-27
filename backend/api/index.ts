8942fd879ce91054c6de596940a0be59f7802f29
Ezetorc <ezetorc@gmail.com>
2026-04-26 22:52:16 -0300

    change entry point

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,11 +1,11 @@
 import express, { json as jsonMiddleware } from 'express'
-import { env } from './configuration/env.configuration.js'
 import { AuthRouter } from './routers/auth.router.js'
 import { UsersRouter } from './routers/users.router.js'
 import { ArticlesRouter } from './routers/articles.router.js'
 import corsMiddleware from 'cors'
 import { errorHandlerMiddleware } from './middlewares/error-handler.middleware.js'
 import { CommentsRouter } from './routers/comments.router.js'
+import serverless from "serverless-http"
 
 const app = express()
 
@@ -17,10 +17,12 @@ app.use('/articles', ArticlesRouter)
 app.use('/comments', CommentsRouter)
 app.use(errorHandlerMiddleware())
 
-app.listen(env.port, (error) => {
-	if (error) {
-		console.error('Error during server start: ', error)
-	} else {
-		console.log(`Server is running on http://localhost:${env.port}`)
-	}
-})
+// app.listen(env.port, (error) => {
+// 	if (error) {
+// 		console.error('Error during server start: ', error)
+// 	} else {
+// 		console.log(`Server is running on http://localhost:${env.port}`)
+// 	}
+// })
+
+export default serverless(app)
