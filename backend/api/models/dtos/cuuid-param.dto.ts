8c5f9f91f62067b00c6e7b5e227470516b3ab530
Ezetorc <ezetorc@gmail.com>
2026-04-27 22:24:37 -0300

    add regex instead of cuid

--- backend/api/models/dtos/cuuid-param.dto.ts
+++ backend/api/models/dtos/cuuid-param.dto.ts
@@ -1,5 +1,5 @@
 import * as z from 'zod'
 
 export const CUIDParamDto = z.object({
-	id: z.cuid()
+	id: z.string().regex(/^[a-z0-9]+$/i)
 })
--- backend/api/models/dtos/omit-id-param.dto.ts
+++ backend/api/models/dtos/omit-id-param.dto.ts
@@ -1,5 +1,5 @@
 import * as z from 'zod'
 
 export const OmitIdParamDto = z.object({
-	omit: z.cuid()
+	omit: z.string().regex(/^[a-z0-9]+$/i)
 })
