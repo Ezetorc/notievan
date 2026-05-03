4faf733e5cfe9f36d63c03c095d522d88d181d4a
Ezetorc <ezetorc@gmail.com>
2026-05-03 18:24:45 -0300

    made pagination params optional

--- shared/src/dtos/in/pagination-params.dto.ts
+++ shared/src/dtos/in/pagination-params.dto.ts
@@ -1,12 +1,13 @@
 import * as z from 'zod'
 
 export const PaginationParamsDto = z.object({
-	limit: z.preprocess(
-		(val) => (val === undefined ? 4 : Number(val)),
-		z.number().min(1)
-	),
-	page: z.preprocess(
-		(val) => (val === undefined ? 1 : Number(val)),
-		z.number().min(1)
-	)
+  limit: z.preprocess(
+    (val) => (val !== undefined ? Number(val) : undefined),
+    z.number().min(1).optional()
+  ).default(4),
+
+  page: z.preprocess(
+    (val) => (val !== undefined ? Number(val) : undefined),
+    z.number().min(1).optional()
+  ).default(1)
 })
