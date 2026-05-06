553726b97f116e461cad292858b3e2927082d9ef
Ezetorc <ezetorc@gmail.com>
2026-05-06 19:44:28 -0300

    fix env variable bug

--- frontend/src/configuration/env.configuration.ts
+++ frontend/src/configuration/env.configuration.ts
@@ -1,24 +1,23 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-	API_URL: z
-		.url()
-		.default('http://localhost:3000')
-		.transform((url) => url.replace(/\/+$/, ''))
+  VITE_API_URL: z
+    .url()
+    .default('http://localhost:3000')
+    .transform((url) => url.replace(/\/+$/, ''))
 })
 
-const parsed = envSchema.safeParse(import.meta)
+const parsed = envSchema.safeParse(import.meta.env)
 
 if (!parsed.success) {
-	console.error(
-		'❌ Invalid environment variables:',
-		z.treeifyError(parsed.error).properties
-	)
-	throw new Error('Invalid environment variables')
+  console.error(
+    '❌ Invalid environment variables:',
+    z.treeifyError(parsed.error).properties
+  )
+
+  throw new Error('Invalid environment variables')
 }
 
-const { API_URL } = parsed.data
-
 export const env = {
-	baseUrl: API_URL
+  baseUrl: parsed.data.VITE_API_URL
 }
